25afc1ff98a425975223aa313d8e9504c14f3940
Evgeny Belkin <sunafajro@gmail.com>
2017-08-04 00:04:03 +0300

    селект наполняется автоматически из пропов

--- src/select.js
+++ src/select.js
@@ -1,24 +1,18 @@
 import React from "react";
 
-export default ({ term, update }) => {
+export default ({ term, update, name, options }) => {
 
   const dataSearch = e => {
     const value = e.target.value;
-
-    update('city', value);
-    
+    update(name, value);    
   };
 
   return (
     <div className="form-group">
       <select className="form-control input-sm" onChange={dataSearch} value={ term }>
-        <option value="all" disabled="true">-все города-</option>
-        <option value="1">Чебоксары</option>
-        <option value="2">Новочебоксарск</option>
-        <option value="3">общешкольные</option>
-        <option value="4">Кугеси</option>
-        <option value="5">Казань</option>
-        <option value="6">Канаш</option>        
+        { options.map(item =>
+            <option key={ item.key } value={ item.key }>{ item.value }</option>
+        ) }               
       </select>
     </div>
   );
