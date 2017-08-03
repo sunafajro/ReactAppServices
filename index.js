e63d54b6890d2e43850522fd9adeee99184c2878
Evgeny Belkin <sunafajro@gmail.com>
2017-08-04 00:02:32 +0300

    добавляем объект с фильтрами и передаем их в компонент

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ import App from './src/app';
 
 const tableHeader = window.tableHeader;
 const tableData = window.tableData;
+const dataFilters = window.dataFilters; 
 
 ReactDOM.render(
-  <App data={ tableData } header={ tableHeader } />,
+  <App data={ tableData } header={ tableHeader } filters={ dataFilters } />,
   document.getElementById('react-root')
 );
