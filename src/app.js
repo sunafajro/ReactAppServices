e56f9c0c48ceedace617f8cf993172beb00ad246
Evgeny Belkin <sunafajro@gmail.com>
2017-08-04 22:52:29 +0300

    добавляем функцию открытия модального окна

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Table from './table';
 import Input from './input';
 import Select from './select';
+import Modal from './modal';
 
 export default class App extends React.Component {
 
@@ -18,8 +19,10 @@ export default class App extends React.Component {
           eduage_id: '',
           eduform_id: ''
       },
+      formElements: {},
       header: this.props.header,
       filteredData: [],
+      error: false
     };
 
   }
@@ -28,7 +31,7 @@ export default class App extends React.Component {
     this.setState({ filteredData: this.state.data });
   }
 
-  updateData = (key, value) => {
+  updateState = (key, value) => {
     let filteredItems = this.state.data;
     let newFilter = {...this.state.filter};
 
@@ -50,51 +53,70 @@ export default class App extends React.Component {
     
   }
 
+  showModal = () => {
+    // fetch('http://localhost:8000/service/create',
+    // { 
+    //   include: 'credentials'
+    // })
+    // .then((response) => response.json())
+    // .then(json => {
+    //   this.setState({ formElements: JSON.parse(json) });
+    // })
+    // .catch(err => {
+    //   this.setState({ error: true });
+    // });
+
+    $('#service-modal').modal('show');
+  }
+
   render() {
     return (
       <div>
         <div id="sidebar" className="col-sm-2">
           <h4>Действия</h4>
-          <button className="btn btn-success btn-sm btn-block"><i className="fa fa-plus" aria-hidden="true"></i> Добавить</button>
+          <button className="btn btn-success btn-sm btn-block" onClick={this.showModal}><i className="fa fa-plus" aria-hidden="true"></i> Добавить</button>
           <h4>Фильтры</h4>
           <Input
+            placeholder="Введите id..."
             term={ this.state.filter.id }
-            update={ this.updateData }
+            name="id"
+            update={ this.updateState }
           />
           <Select
             term={ this.state.filter.city_id }
-            update={ this.updateData }
+            update={ this.updateState }
             name="city_id"
             options={ this.props.filters.cities }
           />
           <Select
             term={ this.state.filter.type_id }
-            update={ this.updateData }
+            update={ this.updateState }
             name="type_id"
             options={ this.props.filters.types }
           />
           <Select
             term={ this.state.filter.language_id }
-            update={ this.updateData }
+            update={ this.updateState }
             name="language_id"
             options={ this.props.filters.languages }
           />
           <Select
             term={ this.state.filter.eduage_id }
-            update={ this.updateData }
+            update={ this.updateState }
             name="eduage_id"
             options={ this.props.filters.eduages }
           />
           <Select
             term={ this.state.filter.eduform_id }
-            update={ this.updateData }
+            update={ this.updateState }
             name="eduform_id"
             options={ this.props.filters.eduforms }
           />
         </div>
         <div id="content" className="col-sm-10">
           <Table data={ this.state.filteredData } header={ this.state.header } />
-          </div>
+        </div>
+        <Modal filters={ this.props.filters } />
       </div>
     );
   }
