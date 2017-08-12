88e1bba28041bd73a662ad5d0c5f5c938c0a0b6b
Evgeny Belkin sunafajro@gmail.com <sunafajro@gmail.com>
2017-08-13 00:12:52 +0300

    убран запрос исходных данных из тела страницы и props

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './src/app';
 
-const tableHeader = window.tableHeader;
-const tableData = window.tableData;
-const dataFilters = window.dataFilters; 
-
 ReactDOM.render(
-  <App data={ tableData } header={ tableHeader } filters={ dataFilters } />,
+  <App />,
   document.getElementById('react-root')
 );
